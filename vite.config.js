b6d2ad11d508fe6fb62655fb473779a3048d7aaa
almero320 <71206946+almero320@users.noreply.github.com>
2026-01-31 03:19:09 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js (di root project)
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: '/', // penting untuk Vercel
+  build: {
+    outDir: 'dist', // pastikan output ke 'dist'
+  },
+});
